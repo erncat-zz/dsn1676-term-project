c5e9a045e683beb0a25be81ca7ee6b3482749895
Erin <monc0032@algonquinlive.com>
2015-03-18 16:49:53 -0400

    2 buttons added
    Js buttons added with animations and style
    small adjustments

--- js/main.js
+++ js/main.js
@@ -4,6 +4,13 @@ var $panel = $('.panel');
 var $btnMove = $('.btn-move');
 var $fish = $('.fish');
 
+var $btnRoll = $('.btn-roll');
+var $yarn = $('.yarn');
+var $yarnMove = $('.yarn-move');
+
+var $btnMouse = $('.btn-mouse');
+var $mouse = $('.mouse');
+
 $toggle.on('click', function () {
 	$panel.toggleClass('js-panel-open');
 });
@@ -12,3 +19,11 @@ $btnMove.on('click', function () {
     $fish.toggleClass('js-fish-move');
 });
 
+$btnRoll.on('click', function () {
+    $yarn.toggleClass('js-yarn-rotate');
+    $yarnMove.toggleClass('js-yarn-move');
+});
+
+$btnMouse.on('click', function () {
+    $mouse.toggleClass('js-mouse-move');
+});
