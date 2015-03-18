5673da2ab8423725a8e9295285abba003b46d296
Erin <monc0032@algonquinlive.com>
2015-03-18 15:51:54 -0400

    Button Made
    js button created and styled

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
 var $toggle = $('.toggle');
 var $panel = $('.panel');
 
+var $btnMove = $('.btn-move');
+var $fish = $('.fish');
+
 $toggle.on('click', function () {
 	$panel.toggleClass('js-panel-open');
 });
+
+$btnMove.on('click', function () {
+    $fish.toggleClass('js-fish-move');
+});
+
